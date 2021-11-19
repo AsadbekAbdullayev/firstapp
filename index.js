dc7e92150e8c297c0ad06c4e2dbf7b014296d255
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-11-19 18:19:32 +0300

    azizbek

---
+++ index.js
@@ -0,0 +1,29 @@
+// const obj = {
+//     a:1,
+//     b:2,
+//     getA(){
+//         console.log(this.a)
+//         let getb = {
+//             getB(){
+//                 console.log(obj.b);
+//             }
+//         }
+//        return getb               
+//     }
+// }
+// obj.getA().getB()
+
+
+// function obj(num1=0,num2=0){
+
+//     if(num1>0 &&num2>0){
+// console.log(num1+num2);
+//     }
+//         return (num3=0)=> {
+//             if(num3>0)
+//             console.log(num1+num2+num3);       
+//     }   
+// }
+
+// obj(1)(2);
+// obj(1,2);
