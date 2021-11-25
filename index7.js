6c844734e5640435b49072ad86cef5d451acd55b
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-11-25 18:52:42 +0300

    15-noyabr

---
+++ index7.js
@@ -0,0 +1,194 @@
+// // const user = {
+// //     id : 1,
+// //     name : 'Web',
+// //     surname : 'Brain',
+// //     key:'123',
+// //     // fullname : function (){
+        
+// //     //     console.log(this.id);
+// //     // }
+// //     data:{
+// //         nma:'tesfddvd',
+// //         getname(){
+// //             console.log(this);
+// //         }
+// //     }
+   
+// // }
+// // console.log(user.data.getname());
+
+// class Student {
+//     constructor(title){
+//         console.log(title);
+//         this.title = title;
+
+//     }
+//     getname(){
+//         console.log(this.title,'Student');
+//     }
+    
+
+// class Test{
+//     constructor(f){
+//         // console.log(f);
+//     }
+//     get(d){
+//         console.log(d);
+//     }
+
+// }
+// new Test().get('ssssssssss')
+
+// class Ree extends Test{
+//     constructor(ff){
+//         console.log(ff);
+//         super()  =======>super bulmasa Testga qiymat berib bulmaydi
+//     }
+// }
+
+// new Ree('dd').get('testdan')
+
+
+
+
+
+
+
+// }
+// class Department extends Student {
+//     constructor(titles){
+//         super(titles)
+//     }
+// }
+// let a = new Department('Asadbek')
+
+// a.getname();
+
+// callback
+// promise
+// async await
+// console.log('Started....');
+// let users = 
+//     {
+//         id:1,
+//         name:'Asadbek',
+//         pw:123
+//     }
+    
+
+
+// const Facebook = (pw,username,callback)=>{
+//     setTimeout(()=>{
+//         if(users.name === username && users.pw === pw){
+//             callback ('Welcome to Facebook')
+//     }else{
+//         callback('Errorrrr')
+//     }},  
+//     1000)
+
+// }
+// Facebook(123,'Asadbek',(data)=>{
+// console.log(data);
+// console.log('Finished');
+
+// })
+// console.log('Started....');
+// let users = 
+//     {
+//         id:1,
+//         name:'Asadbek',
+//         pw:123
+//     }
+
+// const Facebook = (pw,username)=>{
+// let user = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         if(users.name === username && users.pw === pw){
+//             resolve ('Welcome to Facebook')
+
+//     }else{
+//         reject('Errorrrr')
+//     }        
+
+//     },1000)
+// });
+
+// user.then((res)=>console.log(res)).catch((err)=>console.log(err))
+
+// };
+
+// Facebook(123,'Asadbek')
+
+// let a = '**********'.length;
+// let b = '**********'.length;
+// console.log(a*b);
+
+// let a = 'AASWAASEEE';
+// let noRepaet = ()=>{
+//     let b ='';
+//     let c =[];
+//     let sort = a.split('').sort();
+//     for(i=0;i<a.length;i++){
+//         if(b.search(a[i]) === -1){     
+//         c.push(a[i]) 
+//         }
+//         b=c.join('')
+//     }
+// let bb = b.split('');
+// let w =0;
+// let ww =[];
+// for(j=0;j<bb.length;j++){
+//     for(q=0;q<sort.length;q++){
+//         if(bb[j]===sort[q]){
+//             w++;
+
+//         }
+//     }
+//     ww.push(w,b[j])
+    
+//     w=0;
+// }
+// console.log(ww.join(""))
+
+// }
+// noRepaet();
+
+// function obj(num1=0,num2=0){
+//     console.log(num1+num2);
+//   return function(num3=0){
+//       if(num3>0){
+//         console.log(num1+num3);
+//       }
+//   }
+// }
+
+
+// const obj = {
+//     a:1,
+//     b:2,
+//     getA(){
+//         console.log(this.a)
+//         let getb = {
+//             getB(){
+//                 console.log(obj.b);
+//             }
+//         }
+//        return getb               
+//     }
+// }
+// obj.getA().getB()
+
+
+// function obj(num1=0,num2=0){
+
+//     if(num1>0 &&num2>0){
+// console.log(num1+num2);
+//     }
+//         return (num3=0)=> {
+//             if(num3>0)
+//             console.log(num1+num2+num3);       
+//     }   
+// }
+
+// obj(1)(2);
+// obj(1,2);
--- index9.js
+++ index9.js
@@ -1,20 +1,50 @@
-const a = [
-    {
-        id:1,
-        name:'Asadbek',
-        status:'grant'
-    },
-    {
-        id:2,
-        name:'Xolmirza',
-        status:'kantract'
-    },
-    {
-        id:3,
-        name:'Olim',
-        status:'grant'
-    },
+// const user = [
+//     {
+//         id:1,
+//         name:'Asadbek',
+//         status:'grant'
+//     },
+//     {
+//         id:2,
+//         name:'Xolmirza',
+//         status:'kantract'
+//     },
+//     {
+//         id:3,
+//         name:'Olim',
+//         status:'grant'
+//     },
     
-]
-let b = [1,2,3,4,3,3,4,4,6,6,5,5];
-console.log(b.filter((value,index)=>b.indexOf(index)));
+// ]
+// let search=(up)=>{
+//     if(up.length!==0){
+//     user.map((value)=>{
+//         if(value.name.includes(up) || value.status.includes(up) ){
+//             console.log(value);
+//         }
+//     })}else(console.log('malumot kiritin'))
+// }
+// search('za');
+
+// let b = [1,2,3,4,3,3,4,4,6,6,5,5];
+// console.log(b.filter((value,index)=>b.indexOf(value)===index));
+
+// let a = 'We32r2e3';
+// let sum=0;
+
+// let r = a.split('').filter((value)=>parseInt(value)).map((v)=>{
+//     sum=parseInt(v)+sum
+// }); 
+// console.log(sum);
+// FINAL EXAM
+
+// console.log(Number(11).toString(5)); 11 sonini 5likga utqazadi
+
+// console.log(parseInt(21,5)); 5likdgi 21 sonini 10likga utqazadi
+
+// console.log(eval('12+3'));
+
+// ?, :, every
+
+// let a = ['salom','olim','dew']
+// a.map(([value,key],index)=>console.log(value))
