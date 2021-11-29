6bc284be38c3d76885e99cbd6d3f99590cfb4608
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-11-30 00:36:48 +0300

    30

---
+++ firstapp/src/App.js
@@ -0,0 +1,15 @@
+import Farzan from './Farzand';
+import './farzand.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<Farzan/>
+       
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ firstapp/src/Farzand.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Farzand extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Salom man farzandanman</h1>
+            </div>
+        )
+        }
+}
+
+export default Farzand;
