7f43addcf128a3b3d72ad9e500fbae89b097927d
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-12-06 12:52:42 +0300

    s

--- firstapp/src/Student.js
+++ firstapp/src/Student.js
@@ -4,7 +4,9 @@ import React from "react";
      constructor(props){
          super(props)
              this.state={
-                count:0
+                count:0,
+                name:'',
+                surname:''
              }
         
      }
@@ -19,14 +21,36 @@ import React from "react";
             count:this.state.count + 1
         })
     }
+    const in1 = (e)=>{
+       
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+        
+
+    }
+    
          
          return(
              <div className='card'>
                  <h1 onClick={add1}>-</h1>
-                 <h1>{this.state.count}</h1>
+                 <h1>COUNT :{this.state.count}  </h1>
+                 <h1>NAME :{this.state.name}  </h1>
+                 <h1>SURNMAE :{this.state.surname}  </h1>
                  <h1 onClick={add2}>+</h1>
+                <input name='name' type='text' placeholder= 'name' onChange={in1} value={this.state.name}/>
+                <input name='surname' type='text' placeholder= 'surname' onChange={in1} value={this.state.surname}/>
              </div>
          )
      }
  }
  export default Student;
+
+
+
+
+
+
+
+
+ 
