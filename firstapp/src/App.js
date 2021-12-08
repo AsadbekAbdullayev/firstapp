75b4c360ba81244d8bc7b77b681688275777b329
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2021-12-08 06:01:44 +0300

    8-dekabr

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import './App.css'
-import Student from "./Student";
+import React  from "react";
+import Student from './Student';
+import './App.css';
 
 class App extends React.Component{
 	render(){
 		return(
 			<div>
-			<Student/>
-		
+				<Student/>
 			</div>
 		)
 	}
 }
-
-
-
 export default App;
-
 
